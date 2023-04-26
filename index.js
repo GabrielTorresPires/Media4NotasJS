2ccbca16ffb1e78770509c83b30ceb9ef0906de9
Gabriel Torres <gabrieltlpires@gmail.com>
2023-04-26 20:38:53 -0300

    Soluciona o exercício JS

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+alert('-----Média-----');
+let n1 = parseFloat(prompt('Nota 1:'));
+let n2 = parseFloat(prompt('Nota 2:'));
+let n3 = parseFloat(prompt('Nota 3:'));
+let n4 = parseFloat(prompt('Nota 4:'));
+
+const resultado = (n1+n2+n3+n4)/4;
+ alert (`Sua média é: ${resultado}`); 
+
+
+
+if (resultado >= 6)
+{
+    alert('Parabéns você foi aprovado😻');
+}
+else 
+{
+    alert('Reprovado😫');
+}
